24d114dfb0992d831885891701f8a28594579912
ihatena45 <dimasinka010@gmail.com>
2023-11-28 10:03:17 +0700

    update

---
+++ stores/category.ts
@@ -0,0 +1,57 @@
+import { defineStore } from "pinia";
+
+export const useCategoryStore = defineStore("category", {
+    state: () => ({
+        categories: {} as any,
+        status: false,
+        message: "",
+    }),
+    actions: {
+        async getAllCategory() {
+            const { baseUrl, apikey } = useAppConfig();
+            const { data, error } = await useFetch("/rest/v1/categories", {
+                baseURL: baseUrl,
+                method: "GET",
+                headers: {
+                    apikey: apikey,
+                },
+            })
+
+            if (error.value) {
+                this.status = false;
+                this.message = "Get Products Failed !!!";
+            } else if (data) {
+                this.status = true;
+                this.message = "Get Products successfully";
+                this.categories = data.value;
+            }
+        },
+
+        async createCategory(categoryData: any) {
+            try {
+                const { baseUrl, apikey } = useAppConfig();
+                const { data, error } = await useFetch("/rest/v1/categories", {
+                    baseURL: baseUrl,
+                    method: "POST",
+                    headers: {
+                        apikey: apikey,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(categoryData),
+                });
+
+                if (error.value) {
+                    this.status = false;
+                    this.message = "Create Category Failed !!!";
+                } else if (data) {
+                    this.status = true;
+                    this.message = "Create Category successfully";
+                    this.categories = data.value;
+                }
+            } catch (error) {
+                // Tangani kesalahan jika diperlukan
+                console.error(error);
+            }
+        },
+    },
+})
